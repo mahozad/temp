a3859d02f1cc1dc774563eec9f025227acba6ce7
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2021-02-05 20:34:08 +0330

    Add a note about JVM targets

--- action.main.kts
+++ action.main.kts
@@ -1,3 +1,4 @@
+// NOTE: Targets > 1.8 do not work. Is the problem from https://github.com/fwilhe2/setup-kotlin action?
 @file:CompilerOptions("-jvm-target", "1.8")
 
 println("This is TODO to Issue converter!")
